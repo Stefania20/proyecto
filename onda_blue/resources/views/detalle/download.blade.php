1dcc061ada059b8c5249d946a67d17e2cc6aa2d1
Your Name <you@example.com>
2022-12-05 17:27:53 -0500

    pdf

--- onda_blue/resources/views/detalle/download.blade.php
+++ onda_blue/resources/views/detalle/download.blade.php
@@ -1,10 +1,17 @@
-<hr>
-        @if(isset($facturaD))
-        <div class="hol">
-            Factura: {{ $facturaD->id }}
-        </div>
-        <br>
-        <table class='table'>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDF</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+<body>
+<table class='table'>
             <tr>
                 <th>Prenda</th>
                 <th>Tipo tela</th>
@@ -12,50 +19,53 @@
                 <th>Descripcion</th>
                 <th>Cantidad</th>
                 <th>Precio</th>
-                <th></th>
-                <th></th>
+                
 
             </tr>
-            @php
-            $total=0
-
-            @endphp
-            @if(isset($detalles))
-            @foreach($detalles as $detalle)
+            
             <tr>
-                <td>{{ $detalle->prendas->nombre }} </td>
-                <td>{{ $detalle->prendas->tipo_tela }}</td>
-                <td>{{ $detalle->prendas->color }}</td>
-                <td>{{ $detalle->descripcion}} </td>
-                <td>{{ $detalle->cantidad }} </td>
-                <td>{{ $detalle->precio }} </td>
-
-                <td>
-                    <a href="{{ route ( 'detalles.edit', $detalle->id ) }} ">
-                        <button class="btn btn-outline-warning" onclick="return confirm('¿Quieres Editar?')">Editar</button>
-                    </a>
-                </td>
-                <td>
-                    <form action="{{url('detalles/'.$detalle->id)}}" method="post">
-                        @csrf
-                        {{method_field('DELETE')}}
-                        <button class="btn btn-outline-danger" type="submit" onclick="return confirm('¿Quieres Eliminar?')" value="Eliminar"> Eliminar </button>
-                    </form>
-                </td>
-
+                <td>pantalon </td>
+                <td>lino</td>
+                <td>rojo</td>
+                <td>la prenda tiene rotos</td>
+                <td>1 </td>
+                <td>6000 </td>
             </tr>
-            @php
-            $total=$total+($detalle->cantidad*$detalle->precio )
-            @endphp
-            @endforeach
-            @endif
+             
             <tr>
-                <th>Total: {{$total}} </th>
+                <td>pantalon </td>
+                <td>lino</td>
+                <td>rojo</td>
+                <td>manchado</td>
+                <td>2 </td>
+                <td>5000 </td>
             </tr>
-            <div class="col-xl-12 text-right">
-                  <a href="{{ route('download-pdf') }}" class="btn btn-success btn-sm">PDF</a>
-              </div>
-        </table>
+            <tr>
+                <td>saco </td>
+                <td>gamusa</td>
+                <td>azul</td>
+                <td>la prenda tiene rotos</td>
+                <td>2 </td>
+                <td>8000 </td>
+            </tr>
+            <tr>
+                <td>saco </td>
+                <td>gamusa</td>
+                <td>azul</td>
+                <td>prenda  manchada</td>
+                <td>1 </td>
+                <td>8000 </td>
+            </tr>
+          
+          
+            <tr>
+                <th>Total:40000  </th>
+            </tr>
+</table>
 
+    
 
-@endif
+   
+      
+</body>
+</html>
